d7171787bc6981685ad34a95a5a2b155f0cd1198
Tobias Alfonso <143675194+El-Tobi10@users.noreply.github.com>
2024-06-20 02:20:40 -0300

    Delete Biblioteca.java

--- Biblioteca.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface Biblioteca {
-    public int cantJuegos();
-    public void queJuegosTenes();
-    public List<String> filtroNombre(String nombreJuego);
-    public List<String> filtroGenero(String nombreGenero);
-}
