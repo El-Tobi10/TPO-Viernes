e1b26e89c5ad6bd88d6515c8f870529d69933b2c
Tobias Alfonso <143675194+El-Tobi10@users.noreply.github.com>
2024-06-25 15:17:08 -0300

    Update Codigo_principal.sql

--- src/Codigo_principal.sql
+++ src/Codigo_principal.sql
@@ -15,85 +15,27 @@ CREATE TABLE usuarios(
 	creacion date,
 	);
 
-CREATE TABLE juegos (
-    id_juego int PRIMARY KEY IDENTITY,
-    titulo varchar(100) NOT NULL,
-    lanzamiento DATE NOT NULL,
-    desarrollador varchar(50) NOT NULL,
-	);
-
 CREATE TABLE generos(
 	id_genero int PRIMARY KEY IDENTITY,
 	genero varchar(50) NOT NUll
 	);
 
-CREATE TABLE plataformas(
-	id_plataforma int PRIMARY KEY IDENTITY,
-	plataforma varchar(50) NOT NULL
+CREATE TABLE juegos (
+    id_juego int PRIMARY KEY IDENTITY,
+    titulo varchar(100) NOT NULL,
+	id_genero int foreign key references generos(id_genero),
+    lanzamiento DATE NOT NULL,
+    desarrollador varchar(50) NOT NULL,
 	);
 
 go
 
--- Insertado de videojuegos en la tabla Juegos
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('The Legend of Zelda: Breath of the Wild', '2017-03-03', 'Nintendo');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Super Mario Odyssey', '2017-10-27', 'Nintendo');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Red Dead Redemption 2', '2018-10-26', 'Rockstar Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('The Witcher 3: Wild Hunt', '2015-05-19', 'CD Projekt Red');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('God of War', '2018-04-20', 'Santa Monica Studio');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Horizon Zero Dawn', '2017-02-28', 'Guerrilla Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Persona 5', '2016-09-15', 'Atlus');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Bloodborne', '2015-03-24', 'FromSoftware');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Sekiro: Shadows Die Twice', '2019-03-22', 'FromSoftware');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Spider-Man', '2018-09-07', 'Insomniac Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Minecraft', '2011-11-18', 'Mojang');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Fortnite', '2017-07-25', 'Epic Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Overwatch', '2016-05-24', 'Blizzard Entertainment');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Apex Legends', '2019-02-04', 'Respawn Entertainment');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Cyberpunk 2077', '2020-12-10', 'CD Projekt Red');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Animal Crossing: New Horizons', '2020-03-20', 'Nintendo');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Doom Eternal', '2020-03-20', 'id Software');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Half-Life: Alyx', '2020-03-23', 'Valve');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Ghost of Tsushima', '2020-07-17', 'Sucker Punch Productions');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Final Fantasy VII Remake', '2020-04-10', 'Square Enix');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Resident Evil 2', '2019-01-25', 'Capcom');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Monster Hunter: World', '2018-01-26', 'Capcom');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Nier: Automata', '2017-02-23', 'PlatinumGames');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Dark Souls III', '2016-03-24', 'FromSoftware');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Assassin\s Creed Valhalla', '2020-11-10', 'Ubisoft');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('The Last of Us Part II', '2020-06-19', 'Naughty Dog');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Among Us', '2018-06-15', 'InnerSloth');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Genshin Impact', '2020-09-28', 'miHoYo');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Hades', '2020-09-17', 'Supergiant Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Control', '2019-08-27', 'Remedy Entertainment');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Death Stranding', '2019-11-08', 'Kojima Productions');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Ori and the Will of the Wisps', '2020-03-11', 'Moon Studios');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Hollow Knight', '2017-02-24', 'Team Cherry');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Cuphead', '2017-09-29', 'Studio MDHR');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Celeste', '2018-01-25', 'Maddy Makes Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Dead Cells', '2018-08-07', 'Motion Twin');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Slay the Spire', '2019-01-23', 'MegaCrit');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Baba Is You', '2019-03-13', 'Hempuli');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Outer Wilds', '2019-05-28', 'Mobius Digital');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Firewatch', '2016-02-09', 'Campo Santo');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Stardew Valley', '2016-02-26', 'ConcernedApe');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Undertale', '2015-09-15', 'Toby Fox');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Rocket League', '2015-07-07', 'Psyonix');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('PUBG', '2017-12-20', 'PUBG Corporation');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('League of Legends', '2009-10-27', 'Riot Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Valorant', '2020-06-02', 'Riot Games');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Destiny 2', '2017-09-06', 'Bungie');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('FIFA 21', '2020-10-09', 'EA Sports');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('NBA 2K21', '2020-09-04', 'Visual Concepts');
-INSERT INTO juegos (titulo, lanzamiento, desarrollador) VALUES ('Fallout 4', '2015-11-10', 'Bethesda Game Studios');
-select * from juegos;
-go
-
 --Ingreso de los generos
-INSERT INTO generos (genero) VALUES ('Acci髇');
+INSERT INTO generos (genero) VALUES ('Acci贸n');
 INSERT INTO generos (genero) VALUES ('Aventura');
 INSERT INTO generos (genero) VALUES ('Rol');
 INSERT INTO generos (genero) VALUES ('Estrategia');
-INSERT INTO generos (genero) VALUES ('Simulaci髇');
+INSERT INTO generos (genero) VALUES ('Simulaci贸n');
 INSERT INTO generos (genero) VALUES ('Deportes');
 INSERT INTO generos (genero) VALUES ('Carreras');
 INSERT INTO generos (genero) VALUES ('Pelea');
@@ -114,12 +56,12 @@ INSERT INTO generos (genero) VALUES ('Metroidvania');
 INSERT INTO generos (genero) VALUES ('JRPG');
 INSERT INTO generos (genero) VALUES ('Dungeon Crawler');
 INSERT INTO generos (genero) VALUES ('Stealth');
-INSERT INTO generos (genero) VALUES ('Ciencia ficci髇');
-INSERT INTO generos (genero) VALUES ('Fant醩tico');
+INSERT INTO generos (genero) VALUES ('Ciencia ficci贸n');
+INSERT INTO generos (genero) VALUES ('Fant谩stico');
 INSERT INTO generos (genero) VALUES ('Western');
 INSERT INTO generos (genero) VALUES ('Cyberpunk');
-INSERT INTO generos (genero) VALUES ('Post-apocal韕tico');
-INSERT INTO generos (genero) VALUES ('Hist髍ico');
+INSERT INTO generos (genero) VALUES ('Post-apocal铆ptico');
+INSERT INTO generos (genero) VALUES ('Hist贸rico');
 INSERT INTO generos (genero) VALUES ('Educativo');
 INSERT INTO generos (genero) VALUES ('Indie');
 INSERT INTO generos (genero) VALUES ('Arcade');
@@ -127,22 +69,166 @@ INSERT INTO generos (genero) VALUES ('Realidad virtual');
 INSERT INTO generos (genero) VALUES ('Realidad aumentada');
 INSERT INTO generos (genero) VALUES ('Novela visual');
 INSERT INTO generos (genero) VALUES ('Ritmo');
-INSERT INTO generos (genero) VALUES ('T醕tico');
+INSERT INTO generos (genero) VALUES ('T谩ctico');
 INSERT INTO generos (genero) VALUES ('Sandbox');
 INSERT INTO generos (genero) VALUES ('Party game');
-INSERT INTO generos (genero) VALUES ('Horror psicol骻ico');
+INSERT INTO generos (genero) VALUES ('Horror psicol贸gico');
 INSERT INTO generos (genero) VALUES ('Pinball');
 INSERT INTO generos (genero) VALUES ('Tycoon');
 INSERT INTO generos (genero) VALUES ('Survival');
 INSERT INTO generos (genero) VALUES ('JRPG por turnos');
 INSERT INTO generos (genero) VALUES ('Puzzle-platformer');
 select * from generos;
+select * from juegos;
 go 
 
---Ingreso las plataformas
-INSERT INTO plataformas (plataforma) VALUES ('PlayStation');
-INSERT INTO plataformas (plataforma) VALUES ('Xbox');
-INSERT INTO plataformas (plataforma) VALUES ('Nintendo');
-INSERT INTO plataformas (plataforma) VALUES ('PC');
-select * from plataformas;
-go
+-- Insertado de videojuegos en la tabla Juegos
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('The Last of Us Part II', 21, '2020-06-19', 'Naughty Dog');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Cyberpunk 2077', 28, '2020-12-10', 'CD Projekt Red');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Super Mario Odyssey', 9, '2017-10-27', 'Nintendo');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Red Dead Redemption 2', 27, '2018-10-26', 'Rockstar Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('God of War', 7, '2018-04-20', 'Santa Monica Studio');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Horizon Zero Dawn', 13, '2017-02-28', 'Guerrilla Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Persona 5', 24, '2016-09-15', 'Atlus');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Bloodborne', 14, '2015-03-24', 'FromSoftware');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Sekiro: Shadows Die Twice', 28, '2019-03-22', 'FromSoftware');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Spider-Man', 1, '2018-09-07', 'Insomniac Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Minecraft', 36, '2011-11-18', 'Mojang');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Fortnite', 16, '2017-07-25', 'Epic Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Overwatch', 12, '2016-05-24', 'Blizzard Entertainment');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Apex Legends', 16, '2019-02-04', 'Respawn Entertainment');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Animal Crossing: New Horizons', 5, '2020-03-20', 'Nintendo');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Doom Eternal', 12, '2020-03-20', 'id Software');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Half-Life: Alyx', 34, '2020-03-23', 'Valve');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Ghost of Tsushima', 1, '2020-07-17', 'Sucker Punch Productions');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Final Fantasy VII Remake', 24, '2020-04-10', 'Square Enix');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Resident Evil 2', 14, '2019-01-25', 'Capcom');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Monster Hunter: World', 7, '2018-01-26', 'Capcom');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Nier: Automata', 24, '2017-02-23', 'PlatinumGames');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Dark Souls III', 7, '2016-03-24', 'FromSoftware');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Assassins Creed Valhalla', 1, '2020-11-10', 'Bugisoft');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Among Us', 33, '2018-06-15', 'InnerSloth');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Genshin Impact', 24, '2020-09-28', 'miHoYo');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Hades', 18, '2020-09-17', 'Supergiant Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Control', 26, '2019-08-27', 'Remedy Entertainment');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Death Stranding', 26, '2019-11-08', 'Kojima Productions');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Ori and the Will of the Wisps', 9, '2020-03-11', 'Moon Studios');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Hollow Knight', 29, '2017-02-24', 'Team Cherry');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Cuphead', 29, '2017-09-29', 'Studio MDHR');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Celeste', 29, '2018-01-25', 'Maddy Makes Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Dead Cells', 35, '2018-08-07', 'Motion Twin');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Slay the Spire', 19, '2019-01-23', 'MegaCrit');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Baba Is You', 31, '2019-03-13', 'Hempuli');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Outer Wilds', 30, '2019-05-28', 'Mobius Digital');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Firewatch', 2, '2016-02-09', 'Campo Santo');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Stardew Valley', 5, '2016-02-26', 'ConcernedApe');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Undertale', 36, '2015-09-15', 'Toby Fox');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Rocket League', 6, '2015-07-07', 'Psyonix');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('PUBG', 16, '2017-12-20', 'PUBG Corporation');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('League of Legends', 18, '2009-10-27', 'Riot Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Valorant', 12, '2020-06-02', 'Riot Games');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Destiny 2', 17, '2017-09-06', 'Bungie');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('FIFA 21', 6, '2020-10-09', 'EA Sports');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('NBA 2K21', 6, '2020-09-04', 'Visual Concepts');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Fallout 4', 26, '2015-11-10', 'Bethesda Game Studios');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('tom clancys rainbow six siege', 39, '2015-06-5', 'Bugisoft');
+
+INSERT INTO juegos (titulo, id_genero, lanzamiento, desarrollador)
+VALUES ('Rayman 2', 9, '2005-08-22', 'Bugisoft');
