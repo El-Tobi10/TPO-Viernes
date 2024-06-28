ad14242cd8545e14ea03f472f5a2ba51dfa163c3
Gabriel <86844990+gabydomingo@users.noreply.github.com>
2024-06-28 14:11:02 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Usuario tobias = new Usuario("El-Tobi10", "tobias@gmail.com", "12345");
-        //Usuario gaby = new Usuario("El_pendeviejo_erecto", "gabynegrita@hotmail.com", "0725");
+        //Usuario gaby = new Usuario("Gaby0725", "gabriel@hotmail.com", "0725");
         //Usuario chuco = new Usuario("SkibidiSigmaChuco", "peladin@yahoo.com", "pixelgun");
         //tobias.insertarJuegos("Skibidi Toilet", 15, "2023-02-07", "Chuco");
         System.out.println("");
